60afec56e3bff3fd076727c9ac12542cea0d060c
Luv Suneja <Luv@aramex.com>
2023-09-30 12:41:33 +0400

    uml folder rename

--- Calculator-Multi/gui.py
+++ Calculator-Multi/gui.py
@@ -58,5 +58,5 @@ class CalculatorApplication:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = CalculatorApplication(root)
+    app = CalculatorApplication(root, 'normal')
     root.mainloop()
