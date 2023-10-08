b53293dcea7eb04e290c232624ac40347b1ac539
Luv Suneja <Luv@aramex.com>
2023-10-08 10:58:10 +0400

    new commits

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,5 @@
 // src/components/Login.js
-
+import './Login.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -10,35 +10,39 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // For now, just console log the username and password
     console.log('Username:', username, 'Password:', password);
     navigate('/home');
   };
 
   return (
-    <div>
+    <div className="login-container">
+      <div className="header">Bharat Library</div>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className="input-group">
           <label>Username:</label>
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            required
           />
         </div>
-        <div>
+        <div className="input-group">
           <label>Password:</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
+        <button type="submit">Login</button>
       </form>
+      <br />
+      <div>
+        Not registered yet? <a href="/register">Register</a>
+      </div>
     </div>
   );
 };
