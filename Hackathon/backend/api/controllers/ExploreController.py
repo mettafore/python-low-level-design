93002e4a5237c1882d9dff8f263617a7b0fa938a
Luv Suneja <Luv@aramex.com>
2023-10-16 21:00:01 +0400

    book explorere

--- Hackathon/backend/api/controllers/ExploreController.py
+++ Hackathon/backend/api/controllers/ExploreController.py
@@ -8,20 +8,38 @@ GET /books/category/{category_name} - Browse books by category (For Buyers)
 from fastapi import Depends, HTTPException, APIRouter, status, Header
 from services.ExploreService import BooksExplorer
 from framework.database import get_session
+from services.ExploreService import (
+    BookExplorer, 
+    SQLAlchemyDataSource, 
+    GetCategoriesSpecification, 
+    AuthorSpecification, 
+    TitleSpecification, 
+    GenreSpecification
+)
 
 explore_router = APIRouter(prefix="/books", tags=["Explore"])
 
 
 @explore_router.get("/search")
 def search_books(author: str = None, title: str = None, session: Session = Depends(get_session)):
-    strategy = OperationStrategy(SQLAlchemyDataSource(), db)
+    specifications = []
     if author:
-        return strategy.operate(AuthorSpecification(author))
-    elif title:
-        return strategy.operate(TitleSpecification(title))
-    return []
+        specifications.append(AuthorSpecification(author))
+    if title:
+        specifications.append(TitleSpecification(title))
+
+    if len(specifications) > 1:
+        specification = CompositeSpecification(specifications)
+    else:
+        specification = specifications[0]
+    return BookExplorer(SQLAlchemyDataSource(session)).operate(specification)
+
 
 @explore_router.get("/category/{category_name}")
-def browse_books(genre: str, session: Session = Depends(get_session)):
-    strategy = BrowseStrategy(SQLAlchemyDataSource(), db)
-    return strategy.operate(GenreSpecification(category_name))
+def browse_books(session: Session = Depends(get_session)):
+    specification = GenreSpecification(category_name)
+    return BookExplorer(SQLAlchemyDataSource(session)).operate(specification)
+
+@explore_router.get("/categories")
+def get_categories(session: Session = Depends(get_session)):
+    return BookExplorer(SQLAlchemyDataSource(session)).operate(GetCategoriesSpecification())
--- Hackathon/backend/api/services/ExploreService.py
+++ Hackathon/backend/api/services/ExploreService.py
@@ -2,34 +2,104 @@ from abc import ABC, abstractmethod
 from typing import List
 from framework.schema import Book, 
 
-@ABC
-class DataSource:
-    @abstractmethod
-    def search(self, specification: Specification, query: Query) -> List[Book]:
-        pass
-
-    @abstractmethod
-    def browse(self, specification: Specification, query: Query) -> List[Book]:
-        pass
-        
-    
-    def get_categories(self):
-        pass
-
-
-class OperationStrategy:
-    """
-    This class represents a strategy for performing operations on a data source.
-    """
-    def __init__(self, data_source: DataSource, session: Session):
+class BookExplorer:
+    def __init__(self, data_source: DataSource):
         self.data_source = data_source
+
+    def operate(self, specification) -> list:
+        return self.data_source.operate(specification)
+
+
+
+class DataSource(ABC):
+    @abstractmethod
+    def operate(self, specification: Specification):
+        pass
+
+class SQLAlchemyDataSource(DataSource):
+    def __init__(self, session: Session):
         self.session = session
+        self.query = self.session.query(Book)
 
-    def operate(self, specification: Specification) -> List[Book]:
-        query = self.session.query(Book)
-        return self.data_source.search(specification, query)
+    def operate(self, specification: Specification):
+        return specification.is_satisfied_by(self.query)
+    
+class APIDataSource(DataSource):
+    def __init__(self)
+        pass
 
-class BrowseStrategy(OperationStrategy):
-    def operate(self, specification: Specification) -> List[Book]:
-        query = self.session.query(Book)
-        return self.data_source.browse(specification, query)
+    def operate(self, specification: Specification):
+        pass
+
+class Specification(ABC):
+    @abstractmethod
+    def is_satisfied_by(self) -> bool:
+        pass
+
+class AuthorSpecification(Specification):
+    def __init__(self, author: str):
+        self.author = author
+
+    def is_satisfied_by(self, query: Query) -> bool:
+        return query.filter(Book.author == self.author)
+    
+class TitleSpecification(Specification):
+    def __init__(self, title: str):
+        self.title = title
+
+    def is_satisfied_by(self, query: Query) -> bool:
+        return query.filter(Book.title == self.title)
+class GenreSpecification(Specification):
+    def __init__(self, genre: str):
+        self.genre = genre
+
+    def is_satisfied_by(self, query: Query) -> bool:
+        return query.filter(Book.genre == self.genre) 
+    
+class GetCategoriesSpecification(Specification):
+    def __init__(self):
+        pass
+
+    def is_satisfied_by(self, query: Query) -> bool:
+        return query.distinct(Book.genre).all()
+    
+class CompositeSpecification(Specification):
+    def __init__(self, specifications: List[Specification]):
+        self.specifications = specifications
+
+    def is_satisfied_by(self, query: Query) -> bool:
+        for specification in self.specifications:
+            query = specification.is_satisfied_by(query)
+        return query
+
+class APISpecification(Specification):
+    pass
+
+class APIAuthorSpecification(APISpecification):
+    def __init__(self, author):
+        self.author = author
+
+    def is_satisfied_by(self):
+        pass
+
+class APITitleSpecification(APISpecification):
+    def __init__(self, title):
+        self.title = title
+
+    def is_satisfied_by(self):
+        pass
+
+class APIGenreSpecification(APISpecification):
+    def __init__(self, genre):
+        self.genre = genre
+
+    def is_satisfied_by(self):
+        pass
+
+def APICompositeSpecification(APISpecification):
+    def __init__(self, specifications: List[APISpecification]):
+        self.specifications = specifications
+
+    def is_satisfied_by(self):
+        pass 
+
